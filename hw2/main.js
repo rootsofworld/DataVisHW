153254c0ac7bf669adca43ee16d307428f2d92ef
Brave Hsu <bravehsu@Bravede-MacBook-Pro.local>
2017-04-11 11:10:00 +0800

    update

--- hw2/main.js
+++ hw2/main.js
@@ -5,7 +5,7 @@
     
     function login(callback) {
         var CLIENT_ID = 'f784a7131bc746d7973e1edfd5e1de48';
-        var REDIRECT_URI = 'https://rootsofworld.github.io/DataVisHw/spotivis-oauth/';
+        var REDIRECT_URI = 'https://rootsofworld.github.io/DataVisHw//hw2/spotivis-oauth/';
         function getLoginURL(scopes) {
             return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
               '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
